da863073b88c7a23bbccdb24c93d0f4fc8d9ef3a
keunji0708 <33439964+keunji0708@users.noreply.github.com>
2019-01-10 15:24:13 +0900

    Create Square_Fibo.java

--- Fibonacci/src/Recursive_Fibo.java
+++ Fibonacci/src/Recursive_Fibo.java
@@ -4,23 +4,23 @@ public class Recursive_Fibo {
 	public static void main(String args[]) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	
+
 		int N = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i <= N; i++) {
+
+		for (int i = 0; i <= N; i++) {
 			long result = fibo_recur(i);
 			bw.write(i + "번째 피보나치 수 : " + result + "\n");
 		}
-		
+
 		bw.close();
 		br.close();
 	}
+
 	public static long fibo_recur(int N) {
-		if(N <= 1) {
+		if (N <= 1) {
 			return N;
-		}
-		else {
-			return fibo_recur(N-1) + fibo_recur(N-2);
+		} else {
+			return fibo_recur(N - 1) + fibo_recur(N - 2);
 		}
 	}
 }
---
+++ Fibonacci/src/Square_Fibo.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Square_Fibo {
+	public static void main(String args[]) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	}
+}
